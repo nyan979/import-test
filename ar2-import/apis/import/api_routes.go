ae04150e890d96a3c22e74274f0e6127b9d2be44
nyan979 <mirana.979@gmail.com>
2022-09-19 16:17:05 +0800

    cors origin set to r.URL.Host

--- ar2-import/apis/import/api_routes.go
+++ ar2-import/apis/import/api_routes.go
@@ -28,7 +28,7 @@ func (app *Application) routes() http.Handler {
 		// Set CORS headers
 		w.Header().Set("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", r.URL.Host)
 		w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
 
 		// Adjust status code to 204
