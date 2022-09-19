0f53884533020d49dbd43141938ec4591c5a6896
nyan979 <mirana.979@gmail.com>
2022-09-19 15:47:48 +0800

    cors added

--- ar2-import/apis/import/api_routes.go
+++ ar2-import/apis/import/api_routes.go
@@ -26,10 +26,10 @@ func (app *Application) routes() http.Handler {
 
 	router.GlobalOPTIONS = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Set CORS headers
-		header := w.Header()
-		header.Set("Access-Control-Allow-Credentials", "true")
-		header.Set("Access-Control-Allow-Origin", "*")
-		header.Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
 
 		// Adjust status code to 204
 		w.WriteHeader(http.StatusNoContent)
